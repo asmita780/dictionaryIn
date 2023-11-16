4e9132b343c67eef98d76a5ae2e9b41bed23e88d
Asmita <121418396+asmita780@users.noreply.github.com>
2023-11-16 16:52:25 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,21 @@
+
+d={
+    'language': 'python',
+    'fees': 800,
+    'duration':8
+
+}
+
+# # for key+values.........
+# print(d)
+
+# for any individual value.........
+f=d['fees']
+print(f)
+
+# for n in d:
+#     # for keys...
+#     print(n)
+
+    # # for values......
+    # print(d[n])
